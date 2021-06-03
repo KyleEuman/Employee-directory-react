c023eef54fb4a9ed7cccded4e9b32f3c7a67728e
Kyle Euman <euman_kyle@yahoo.com>
2021-06-03 13:31:39 -0400

    added wrapper to components and made changes to api and app js

---
+++ employeedirectory/src/App.js
@@ -0,0 +1,22 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Wrapper from "./components/Wrapper";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Navbar />
+        <Wrapper>
+          <Route  />
+          <Route  />
+          <Route  />
+          <Route  />
+        </Wrapper>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
