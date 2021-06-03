9df78300ef224bb1f0279a2ffb08ecf231c3d303
Kyle Euman <euman_kyle@yahoo.com>
2021-06-03 14:12:38 -0400

    started app.js

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,22 +1,58 @@
-import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/NavBar/NavBar";
-import Wrapper from "./components/Wrapper";
+import React from 'react';
+import EmployeeCard from './components/EmployeeCard';
+import SearchForm from './components/SeachForm';
+import Wrapper from './components/Wrapper';
+import Col from './components/Col';
+import API from './utils/API';
+import './App.css';
 
-function App() {
-  return (
-    <Router>
-      <div>
-        <Navbar />
-        <Wrapper>
-          <Route  />
-          <Route  />
-          <Route  />
-          <Route  />
-        </Wrapper>
-      </div>
-    </Router>
-  );
+class App extends React.Component {
+  state = { employees: [], search: '' };
 }
+componentDidMount() {
+    API.search()
+      .then((res) => {
+        console.log(res);
+        this.setState({
+          employees: res.data.results.map((e, i) => ({
+            firstName: e.name.first,
+            lastName: e.name.last,
+            picture: e.picture.large,
+            email: e.email,
+            phone: e.phone,
+            city: e.location.city,
+            key: i,
+          })),
+        });
+      })
+      .catch((err) => console.log(err));
+  }
+  refreshPage() {
+    window.location.reload(false);
+  }
 
-export default App;
+  searchEmployee = (filter) => {
+    console.log('Search', filter);
+    const filteredList = this.state.employees.filter((employee) => {
+      // merge data together, then check to see if employee exists
+      let values = Object.values(employee).join('').toLowerCase();
+      return values.indexOf(filter.toLowerCase()) !== -1;
+    });
+    // Update the employee list with the filtered value
+    this.setState({ employees: filteredList });
+  };
+
+  handleInputChange = (e) => {
+    // const name = e.target.name;
+    // const value = e.target.value;
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+    console.log('Handle ', this.state.search);
+  };
+
+  handleFormSubmit = (e) => {
+    e.preventDefault();
+    console.log('Button Clicked', this.state.search, e);
+    this.searchEmployee(this.state.search);
+  };
