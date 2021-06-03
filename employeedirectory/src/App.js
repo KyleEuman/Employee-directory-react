f726d38407e80e8d3e60897e09031bea8aab464a
Kyle Euman <euman_kyle@yahoo.com>
2021-06-03 13:53:58 -0400

    added .env finished search form

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-import Navbar from "./components/NavBar";
+import Navbar from "./components/NavBar/NavBar";
 import Wrapper from "./components/Wrapper";
 
 function App() {
