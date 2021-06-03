770fc34913cb465a0478fb93d0402b644875343a
Kyle Euman <euman_kyle@yahoo.com>
2021-06-03 14:35:49 -0400

    got app to work and run

--- employeedirectory/src/App.js
+++ employeedirectory/src/App.js
@@ -1,15 +1,13 @@
 import React from 'react';
-import EmployeeCard from './components/EmployeeCard';
-import SearchForm from './components/SeachForm';
+import EmployeeCard from './components/Employee Card';
+import SearchForm from './components/Search Form';
 import Wrapper from './components/Wrapper';
-import Col from './components/Col';
 import API from './utils/API';
-import './App.css';
 
 class App extends React.Component {
   state = { employees: [], search: '' };
-}
-componentDidMount() {
+
+  componentDidMount() {
     API.search()
       .then((res) => {
         console.log(res);
@@ -27,6 +25,7 @@ componentDidMount() {
       })
       .catch((err) => console.log(err));
   }
+
   refreshPage() {
     window.location.reload(false);
   }
@@ -56,17 +55,53 @@ componentDidMount() {
     console.log('Button Clicked', this.state.search, e);
     this.searchEmployee(this.state.search);
   };
+
   render() {
     return (
       <Wrapper>
         <div className="container">
           <div className="row">
-            <Col size="md-4">
+           
               <h2>Employee Directory</h2>
               <SearchForm
                 value={this.state.search}
                 handleInputChange={this.handleInputChange}
                 handleFormSubmit={this.handleFormSubmit}
               />
-            </Col>
+            
           </div>
+
+          <div className="row">
+            
+              <table className="table">
+                <thead>
+                  <tr>
+                    <th>Photo</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                    <th>City</th>
+                  </tr>
+                </thead>
+                {[...this.state.employees].map((item) => (
+                  <EmployeeCard
+                    picture={item.picture}
+                    firstName={item.firstName}
+                    lastName={item.lastName}
+                    email={item.email}
+                    phone={item.phone}
+                    city={item.city}
+                    key={item.key}
+                  />
+                ))}
+              </table>
+           
+          </div>
+        </div>
+      </Wrapper>
+    );
+  }
+}
+
+export default App;
